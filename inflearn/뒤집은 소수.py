d422cd222440568100ced7300f0e95470a13a87f
ohy123 <zvyg1023@naver.com>
2022-10-15 12:48:04 +0900

    뒤집은 소수

--- inflearn/뒤집은 소수.py
+++ inflearn/뒤집은 소수.py
@@ -1,16 +1,23 @@
 def reverse(x):
-    strNum = str(x)[::-1]
-    return int(strNum)
-
+    res = 0
+    while x>0:
+        res = res * 10 + (x%10)
+        x //= 10
+    return res
 
 def isPrime(x):
-    for j in range(2, x):
+    if x==1:
+        return False
+    for j in range(2, x//2+1):
         if (x % j == 0):
             return False
-    return True
+    else:
+        return True
+
 
 N = int(input())
 numArr = list(map(int, input().split()))
 for k in numArr:
     if (isPrime(reverse(k))):
         print(reverse(k), end=" ")
+
