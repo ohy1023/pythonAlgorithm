307a6fec33c4e2c4a1beb3b37abe002eb6f28e0d
ohy1023 <zvyg1023@naver.com>
2022-11-01 19:46:25 +0900

    인프런 이분검색

---
+++ inflearn/section4/BinarySearch.py
@@ -0,0 +1,16 @@
+n, m = map(int,input().split())
+arr = sorted(list(map(int,input().split())))
+lt = 0
+rt = n -1
+while lt<=rt:
+    mid = (lt+rt) // 2
+    if arr[mid] == m:
+        print(mid+1)
+        break
+    elif arr[mid] > m:
+        rt=mid-1
+    else:
+        lt=mid+1
+
+
+
