52f8f0fecf6cec9748dede5f54ec79045e5ab59b
ohy1023 <zvyg1023@naver.com>
2022-11-07 22:54:37 +0900

    나누어 떨어지는 숫자 배열

---
+++ likelion/algorithm1107/DivisibleArray.py
@@ -0,0 +1,10 @@
+def solution(arr, divisor):
+    answer = []
+    for a in arr:
+        if a % divisor ==0:
+            answer.append(a)
+    if answer == []:
+        answer.append(-1)
+    else:
+        answer.sort()
+    return answer
