911316a39c14cd183d0760aa9a78695f51d6b82f
ohy1023 <zvyg1023@naver.com>
2022-11-08 22:52:27 +0900

    인프런 회의실 배정 그리디

---
+++ inflearn/section4/Meeting.py
@@ -0,0 +1,15 @@
+n = int(input())
+meetings = []
+for i in range(n):
+    s, e = map(int, input().split())
+    meetings.append((s, e))
+
+meetings.sort(key=lambda x: (x[1], x[0]))
+
+et = 0
+cnt = 0
+for s, e in meetings:
+    if s >= et:
+        et = e
+        cnt += 1
+print(cnt)
