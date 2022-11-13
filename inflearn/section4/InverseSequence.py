7dcdc6c21cd732eeb317dd3f14a6ca956a0df7db
ohy1023 <zvyg1023@naver.com>
2022-11-13 14:56:19 +0900

    [인프런] 증가수열 - 그리디 (deque 사용)

---
+++ inflearn/section4/InverseSequence.py
@@ -0,0 +1,15 @@
+k = int(input())
+a = list(map(int, input().split()))
+
+answer = [0] * k
+
+for i in range(k):
+    for j in range(k):
+        if a[i] == 0 and answer[j] == 0:
+            answer[j] = i + 1
+            break
+        elif answer[j] == 0:
+            a[i] -= 1
+
+for i in answer:
+    print(i, end=" ")
