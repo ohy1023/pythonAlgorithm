9b80d71fa7d50d1885f0b329b0f825c1b19c1eca
ohy1023 <zvyg1023@naver.com>
2022-11-18 17:24:21 +0900

    [프로그래머스] 시저 암호

---
+++ likelion/algorithm1118/CaesarCipher.py
@@ -0,0 +1,15 @@
+def solution(s, n):
+    answer = ''
+    for i in s:
+        if i == ' ':
+            answer += i
+        elif i.islower():
+            answer += chr((ord(i) - ord('a') + n) % 26 + ord('a'))
+        elif i.isupper():
+            answer += chr((ord(i) - ord('A') + n) % 26 + ord('A'))
+    return answer
+
+
+s = "abcdefghijklmnop"
+n = 17
+print(solution(s, n))
