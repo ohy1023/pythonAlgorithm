8e5aa08fb1491b27b4ba3954009fc17f7e1e4871
ohy1023 <zvyg1023@naver.com>
2022-11-20 12:48:58 +0900

    [파알인] 유효한 팰린드롬 - 슬라이싱 사용

---
+++ book/stringmanipulation/ValidPalindrome3.py
@@ -0,0 +1,15 @@
+import re
+
+
+class Solution:
+
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub('[^a-z0-9]', '', s)
+
+        return s == s[::-1]
+
+
+answer = Solution()
+
+print(answer.isPalindrome("a car rac a"))
