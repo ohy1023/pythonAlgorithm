c29cb535972a2f2555f914d66adeae1a9ef049e5
ohy1023 <zvyg1023@naver.com>
2022-11-21 11:39:54 +0900

    [인프런] 응급실 - 큐 사용

---
+++ inflearn/section5/EmergencyRoom.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+patient = [(pos, val) for pos, val in enumerate(list(map(int, input().split())))]
+
+patient = deque(patient)
+
+cnt = 0
+
+while True:
+    cur = patient.popleft()
+    if any(cur[1] < x[1] for x in patient):
+        patient.append(cur)
+    else:
+        cnt += 1
+        if cur[0] == m:
+            break
+print(cnt)
