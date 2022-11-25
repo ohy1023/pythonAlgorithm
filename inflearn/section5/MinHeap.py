2daceaaf4f0227f0b63e0e6dec60b9a3b2c7737e
ohy1023 <zvyg1023@naver.com>
2022-11-25 22:55:13 +0900

    [인프런] 최소힙 - 우선순위 큐

--- inflearn/section5/MinHeap.py
+++ inflearn/section5/MinHeap.py
@@ -1,2 +1,11 @@
 from queue import PriorityQueue
 
+hp = PriorityQueue()
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    elif n == 0:
+        print(hp.get())
+    else:
+        hp.put(n)
