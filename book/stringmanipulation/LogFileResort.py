9bd0f07c55663b0d26480df7e486cfe876102050
ohy1023 <zvyg1023@naver.com>
2022-12-05 16:55:21 +0900

    [LeetCode] 로그 파일 재정렬

--- book/stringmanipulation/LogFileResort.py
+++ book/stringmanipulation/LogFileResort.py
@@ -8,10 +8,10 @@ class Solution:
             else:
                 letters.append(log)
         letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
-        print(letters + digits)
+        return letters + digits
 
 
 if __name__ == "__main__":
     logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
     solution = Solution()
-    solution.reorderLogFiles(logs)
+    print(solution.reorderLogFiles(logs))
