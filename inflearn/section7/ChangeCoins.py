347be81d288a8722807d39664ae8162d1cb9bd58
ohy1023 <zvyg1023@naver.com>
2022-12-14 16:38:23 +0900

    [Inflearn] 동전 바꿔주기(DFS)

---
+++ inflearn/section7/ChangeCoins.py
@@ -0,0 +1,24 @@
+def solution(v, s):
+    global cnt
+    # if s > T:
+    #     return
+    if v == k:
+        if s == T:
+            cnt += 1
+    else:
+        for i in range(N[v] + 1):
+            solution(v + 1, s + (P[v] * i))
+
+
+if __name__ == "__main__":
+    T = int(input())
+    k = int(input())
+    P = list()
+    N = list()
+    for _ in range(k):
+        p, n = map(int, input().split())
+        P.append(p)
+        N.append(n)
+    cnt = 0
+    solution(0, 0)
+    print(cnt)
