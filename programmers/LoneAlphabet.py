273846adf5e3bf683ef85275d102941a662b9280
ohy1023 <zvyg1023@naver.com>
2023-01-05 19:33:27 +0900

    [Programmers] 외톨이 알파벳

---
+++ programmers/LoneAlphabet.py
@@ -0,0 +1,24 @@
+def solution(input_string):
+    count = {}
+    answer_list = []
+    for idx, alpha in enumerate(input_string):
+        if alpha not in count:
+            count[alpha] = [idx]
+        else:
+            count[alpha].append(idx)
+    for key, value in count.items():
+        if len(value) >= 2:
+            for i in range(len(value) - 1):
+                if abs(value[i] - value[i + 1]) > 1:
+                    answer_list.append(key)
+                    break
+    if len(answer_list) == 0:
+        answer = "N"
+    else:
+        answer = ''.join(sorted(answer_list))
+
+    return answer
+
+
+if __name__ == "__main__":
+    print(solution(input()))
