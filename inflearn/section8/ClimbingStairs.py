5549eae9f6e424bf733fbb35b06d3045b49ab113
ohy1023 <zvyg1023@naver.com>
2023-02-07 20:21:45 +0900

    [Inflearn] 계단오르기 (Top-Down)

---
+++ inflearn/section8/ClimbingStairs.py
@@ -0,0 +1,16 @@
+import sys
+
+def DFS(num):
+    if dy[num] > 0:
+        return dy[num]
+    if num == 1 or num == 2:
+        return num
+    else:
+        dy[num] = DFS(num - 2) + DFS(num - 1)
+        return dy[num]
+
+
+input = sys.stdin.readline
+n = int(input())
+dy = [0] * (n + 1)
+print(DFS(n))
