4efeab6a0fd5cba73746d9dcd1354aeb304760bd
ohy1023 <zvyg1023@naver.com>
2023-02-23 19:58:06 +0900

    [Inflearn] 가방문제(냅색 알고리즘)

---
+++ inflearn/section8/Bag.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+dy = [0] * (m + 1)
+for i in range(n):
+    w, v = map(int, input().split())
+    for j in range(w, m + 1):
+        dy[j] = max(dy[j - w] + v, dy[j])
+
+print(dy[m])
