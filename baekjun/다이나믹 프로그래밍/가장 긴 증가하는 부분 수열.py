b093ed75befa65798bfccdc1435fbc961a531d5c
ohy1023 <zvyg1023@naver.com>
2023-04-13 14:59:28 +0900

    가장 긴 증가하는 부분 수열 - 다이나믹 프로그래밍

--- baekjun/다이나믹 프로그래밍/가장 긴 증가하는 부분 수열.py
+++ baekjun/다이나믹 프로그래밍/가장 긴 증가하는 부분 수열.py
@@ -7,8 +7,7 @@ if __name__ == "__main__":
     arr = list(map(int, input().split()))
     arr.insert(0, 0)
     dy = [0] * (n + 1)
-    dy[1] = 1
-    res = dy[1]
+    res = dy[1] = 1
     for i in range(2, n + 1):
         max = 0
         for j in range(i - 1, 0, -1):
