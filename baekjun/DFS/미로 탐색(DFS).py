081fe6e0ced784db56b0a1d695d12635629c3b83
ohy1023 <zvyg1023@naver.com>
2023-05-03 12:31:35 +0900

    [백준] 미로 탐색 - DFS (런타임에러)

---
+++ baekjun/DFS/미로 탐색(DFS).py
@@ -0,0 +1,25 @@
+def DFS(x, y):
+    dx = [-1, 0, 1, 0]
+    dy = [0, -1, 0, 1]
+    if x == (n - 1) and y == (m - 1):
+        return
+
+    else:
+        for i in range(4):
+            xx = x + dx[i]
+            yy = y + dy[i]
+
+            if 0 <= xx < n and 0 <= yy < m and board[xx][yy] == 1:
+                if visited[xx][yy] == 0 or visited[xx][yy] > visited[x][y] + 1:
+                    visited[xx][yy] = visited[x][y] + 1
+                    DFS(xx, yy)
+
+        return visited[n - 1][m - 1]
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    board = [list(map(int, input())) for _ in range(n)]
+    visited = [[0] * m for _ in range(n)]
+    visited[0][0] = 1
+    print(DFS(0, 0))
