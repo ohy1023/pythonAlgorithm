a7ef17942665e61b8d2e2a19a7616b9e1d0b32b1
ohy1023 <zvyg1023@naver.com>
2023-05-15 08:53:10 +0900

    [백준] 연구소 - DFS (시간초과)

--- baekjun/DFS/연구소(DFS).py
+++ baekjun/DFS/연구소(DFS).py
@@ -1,30 +1,29 @@
-import copy
 import sys
+import copy
 
-sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
+sys.setrecursionlimit(10 ** 6)
 
-def virus(x, y, temp):
+
+def virus(x, y, arr):
     for i in range(4):
         xx = x + dx[i]
         yy = y + dy[i]
 
-        if 0 <= xx < n and 0 <= yy < m and temp[xx][yy] == 0:
-            temp[xx][yy] = 2
-            virus(xx, yy, temp)
+        if 0 <= xx < n and 0 <= yy < m and arr[xx][yy] == 0:
+            arr[xx][yy] = 2
+            virus(xx, yy, arr)
 
 
 def DFS(v):
     global res
     if v == 3:
         temp = copy.deepcopy(board)
-
         for i in range(n):
             for j in range(m):
                 if temp[i][j] == 2:
                     virus(i, j, temp)
-
         cnt = 0
         for i in range(n):
             cnt += temp[i].count(0)
