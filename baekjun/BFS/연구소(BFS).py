72d5b84b3699251ae62abfed9eb64df5ba0443a3
ohy1023 <zvyg1023@naver.com>
2023-05-15 08:52:52 +0900

    [백준] 연구소 - BFS

--- baekjun/BFS/연구소(BFS).py
+++ baekjun/BFS/연구소(BFS).py
@@ -13,35 +13,32 @@ def BFS():
         for j in range(m):
             if temp[i][j] == 2:
                 dq.append((i, j))
+
     while dq:
         x, y = dq.popleft()
+
         for i in range(4):
             xx = x + dx[i]
             yy = y + dy[i]
-
             if 0 <= xx < n and 0 <= yy < m and temp[xx][yy] == 0:
                 temp[xx][yy] = 2
                 dq.append((xx, yy))
 
-    res = max(get_count(temp), res)
-
-
-def get_count(arr):
     cnt = 0
     for i in range(n):
-        cnt += arr[i].count(0)
-    return cnt
+        cnt += temp[i].count(0)
+    res = max(cnt, res)
 
 
-def make_wall(cnt):
-    if cnt == 3:
+def wall(count):
+    if count == 3:
         BFS()
         return
     for i in range(n):
         for j in range(m):
             if board[i][j] == 0:
                 board[i][j] = 1
-                make_wall(cnt + 1)
+                wall(count + 1)
                 board[i][j] = 0
 
 
@@ -51,5 +48,5 @@ if __name__ == "__main__":
     n, m = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(n)]
     res = 0
-    make_wall(0)
+    wall(0)
     print(res)
