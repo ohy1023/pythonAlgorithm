8de0c5b98dc6bd34d1139bc6d742edbd4a540aed
ohy1023 <110380812+ohy1023@users.noreply.github.com>
2023-05-14 09:12:52 +0900

    [백준] 연구소 - DFS (시간초과)

---
+++ baekjun/DFS/연구소(DFS).py
@@ -0,0 +1,49 @@
+import copy
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+
+def virus(x, y, temp):
+    for i in range(4):
+        xx = x + dx[i]
+        yy = y + dy[i]
+
+        if 0 <= xx < n and 0 <= yy < m and temp[xx][yy] == 0:
+            temp[xx][yy] = 2
+            virus(xx, yy, temp)
+
+
+def DFS(v):
+    global res
+    if v == 3:
+        temp = copy.deepcopy(board)
+
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i, j, temp)
+
+        cnt = 0
+        for i in range(n):
+            cnt += temp[i].count(0)
+        res = max(cnt, res)
+        return
+    else:
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == 0:
+                    board[i][j] = 1
+                    DFS(v + 1)
+                    board[i][j] = 0
+
+
+if __name__ == "__main__":
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    n, m = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    res = 0
+    DFS(0)
+    print(res)
