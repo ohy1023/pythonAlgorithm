fb2a9776596a144b97c1425a459e2882a00fdb6b
ohy1023 <zvyg1023@naver.com>
2023-06-29 09:38:21 +0900

    [백준] 거스름돈 - 그리디

---
+++ baekjun/그리디/거스름돈.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n = int(input())
+
+    cnt = 0
+
+    while n > 0:
+        if n % 5 == 0:
+            cnt += n // 5
+            break
+        else:
+            n -= 2
+            cnt += 1
+
+    if n < 0:
+        print(-1)
+    else:
+        print(cnt)
