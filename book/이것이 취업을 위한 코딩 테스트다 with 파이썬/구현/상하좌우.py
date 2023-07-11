cc473a872b8527788adcd2ee3feefc09241c0130
ohy1023 <zvyg1023@naver.com>
2023-07-11 13:36:14 +0900

    [이코테] 상하좌우 - 구현

--- book/이것이 취업을 위한 코딩 테스트다 with 파이썬/구현/상하좌우.py
+++ book/이것이 취업을 위한 코딩 테스트다 with 파이썬/구현/상하좌우.py
@@ -5,19 +5,14 @@ input = sys.stdin.readline
 if __name__ == "__main__":
     x, y = 1, 1
 
-    move_type = {}
-
-    move_type["L"] = (0, -1)
-    move_type["U"] = (-1, 0)
-    move_type["R"] = (0, 1)
-    move_type["D"] = (1, 0)
+    hash_map = {"L": (0, -1), "U": (-1, 0), "R": (0, 1), "D": (1, 0)}
 
     n = int(input())
     plans = input().split()
 
     for plan in plans:
-        nx = x + move_type[plan][0]
-        ny = y + move_type[plan][1]
+        nx = x + hash_map[plan][0]
+        ny = y + hash_map[plan][1]
 
         if 0 < nx <= n and 0 < ny <= n:
             x, y = nx, ny
