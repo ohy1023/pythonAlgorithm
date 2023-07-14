daed7398d8c61ecc1fcb339ee5446b7a47a156c8
ohy1023 <zvyg1023@naver.com>
2023-07-14 11:41:27 +0900

    [백준] 최소 회의실 개수 - 그리디

---
+++ baekjun/그리디/최소 회의실 개수.py
@@ -0,0 +1,21 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n = int(input())
+    classes = sorted([tuple(map(int, input().split())) for _ in range(n)])
+
+    room = []
+
+    heapq.heappush(room, classes[0][1])
+
+    for i in range(1, n):
+        if classes[i][0] < room[0]:
+            heapq.heappush(room, classes[i][1])
+        else:
+            heapq.heappop(room)
+            heapq.heappush(room, classes[i][1])
+
+    print(len(room))
