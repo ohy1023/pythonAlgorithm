ba2ef66390195071de3c148c14acaab3521fd065
ohy1023 <zvyg1023@naver.com>
2023-07-18 09:37:11 +0900

    [이코테] 미로 탈출 - DFS

---
+++ book/이것이 취업을 위한 코딩 테스트다 with 파이썬/DFS & BFS/미로 탈출 DFS.py
@@ -0,0 +1,39 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def DFS(x, y):
+    global res, cnt
+    if x == (n - 1) and y == (m - 1):
+        if res > cnt:
+            res = cnt
+        return
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and graph[nx][ny] == 1:
+            visited[nx][ny] = True
+            cnt += 1
+            DFS(nx, ny)
+            cnt -= 1
+            visited[nx][ny] = False
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    graph = [list(int(num) for num in input().strip()) for _ in range(n)]
+
+    dx = [-1, 0, 1, 0]
+    dy = [0, -1, 0, 1]
+
+    visited = [[False] * m for _ in range(n)]
+
+    res = 2_147_000_000
+    cnt = 1
+
+    visited[0][0] = True
+    DFS(0, 0)
+
+    print(res)
