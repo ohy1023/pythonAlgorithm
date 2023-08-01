54465e9f8561b3caa27d65b5204b286a2169df5c
ohy1023 <zvyg1023@naver.com>
2023-08-01 18:20:39 +0900

    [이코테] 떡볶이 떡 만들기 - 이진 탐색

--- book/이것이 취업을 위한 코딩 테스트다 with 파이썬/이진 탐색/떡볶이 떡 만들기.py
+++ book/이것이 취업을 위한 코딩 테스트다 with 파이썬/이진 탐색/떡볶이 떡 만들기.py
@@ -6,15 +6,18 @@ if __name__ == "__main__":
     n, m = map(int, input().split())
     rice_cakes = list(map(int, input().split()))
 
+    # 절단기 길이로 이분 탐색 실시 
     lt, rt = 0, max(rice_cakes)
     res = 0
     while lt <= rt:
         mid = (lt + rt) // 2
-
+        # 절단기 길이가 mid일때 얻는 떡 (mid 보다 작으면 음수 가 되므로 제외 후 계산)
         tmp = sum([x - mid for x in rice_cakes if x > mid])
 
+        # 얻은 떡이 목표값보다 작으면 절단기 길이를 줄임
         if tmp < m:
             rt = mid - 1
+        # 얻은 떡이 목표값보다 크거나 같다면 절단기 길이를 느리고 결과값 저장
         else:
             res = mid
             lt = mid + 1
