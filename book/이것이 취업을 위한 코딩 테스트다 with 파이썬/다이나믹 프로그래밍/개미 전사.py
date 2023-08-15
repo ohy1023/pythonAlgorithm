d42473357a6f1f37ffd6a87259b1fd06c37f9e27
ohy1023 <zvyg1023@naver.com>
2023-08-15 17:49:13 +0900

    [이코테] 개미 전사 - DP

--- book/이것이 취업을 위한 코딩 테스트다 with 파이썬/다이나믹 프로그래밍/개미 전사.py
+++ book/이것이 취업을 위한 코딩 테스트다 with 파이썬/다이나믹 프로그래밍/개미 전사.py
@@ -8,9 +8,10 @@ if __name__ == "__main__":
     dy = [0] * 101
 
     # 최대값을 누적하여 저장
-    dy[1] = warehouses[0]  # 첫째 날은 경우의 수가 하나밖에 없으므로 첫 번째 값 저장
-    dy[2] = max(warehouses[0], warehouses[1])  # 연달아 털 수 없으므로 둘째 날은 첫 번째 창고 와 두 번째 창고에서 더 큰 값을 저장
+    dy[1] = warehouses[0]  # 경우의 수가 하나밖에 없으므로 첫 번째 값 저장
+    dy[2] = max(warehouses[0], warehouses[1])  # 연달아 털 수 없으므로 첫 번째 창고 와 두 번째 창고에서 더 큰 값을 저장
     for i in range(3, n + 1):
-        dy[i] = max(dy[i - 2] + warehouses[i - 1], dy[i - 1])  # 이틀 전 값에 현재 창고를 털었을 때의 값과 하루 전의 값과 비교 후 큰 값을 저장
+        dy[i] = max(dy[i - 2] + warehouses[i - 1],
+                    dy[i - 1])  # 한 칸 건너 띄고 현재 창고를 털었을 때의 값과 바로 전의 창고를 털었을 때의 값을 비교 후 큰 값을 저장
 
     print(dy[n])
