7c9d2ba4101b9a5b47c67a2c30a2e2ab96bcfdff
ohy1023 <zvyg1023@naver.com>
2023-08-15 10:20:49 +0900

    [이코테] 개미 전사 - DP

--- book/이것이 취업을 위한 코딩 테스트다 with 파이썬/다이나믹 프로그래밍/개미 전사.py
+++ book/이것이 취업을 위한 코딩 테스트다 with 파이썬/다이나믹 프로그래밍/개미 전사.py
@@ -5,10 +5,12 @@ input = sys.stdin.readline
 if __name__ == "__main__":
     n = int(input())
     warehouses = list(map(int, input().split()))
-    dy = [0] * (n + 1)
+    dy = [0] * n
 
-    dy[0] = warehouses[0]
-    dy[1] = max(warehouses[0], warehouses[1])
+    # [1, 3, 1, 5]
+    # 각 날에 최대값을 저장
+    dy[0] = warehouses[0]  # 첫째 날은 경우의 수가 하나 밖에 없으므로 첫번째 값 저장
+    dy[1] = max(warehouses[0], warehouses[1])  # 둘째 날은 첫번째 창고 와 두번째 창고는 더 큰 값을 저장 - 연달아 털 수 없으므로
     for i in range(2, n):
         dy[i] = max(dy[i - 2] + warehouses[i], dy[i - 1])
 
