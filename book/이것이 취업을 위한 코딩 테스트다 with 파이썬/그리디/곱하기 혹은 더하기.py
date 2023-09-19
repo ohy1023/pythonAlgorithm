ee665897f9c0f0303f3fa8113b75d9a11d599145
ohy1023 <zvyg1023@naver.com>
2023-09-19 16:00:29 +0900

    [이코테] 곱하기 혹은 더하기 - 그리디

--- book/이것이 취업을 위한 코딩 테스트다 with 파이썬/그리디/곱하기 혹은 더하기.py
+++ book/이것이 취업을 위한 코딩 테스트다 with 파이썬/그리디/곱하기 혹은 더하기.py
@@ -1,11 +1,19 @@
-data = input()
+"""
+이코테 level.1
+곱하기 혹은 더하기 - 그리디
+"""
 
-result = int(data[0])
+import sys
 
-for i in range(1,len(data)):
-    num = int(data[i])
-    if num <= 1 or result <=1:
-        result += num
-    else:
-        result *= num
-print(result)
+if __name__ == "__main__":
+    input = sys.stdin.readline
+
+    number = input().strip()
+    result = int(number[0])  # 첫 번째 숫자로 초기화
+
+    for i in range(1, len(number)):
+        num = int(number[i])
+        if num <= 1 or result <= 1:
+            result += num
+        else:
+            result *= num
